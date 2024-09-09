917a074583fb9dc6a58e9642f9a39e9f07a857ad
Rizwan <rizwanfahim24@gmail.com>
2024-09-09 17:20:45 +0600

    Server Running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+// import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import { json } from "body-parser";
+
+// dotenv.config();
+
+const app = express();
+app.use(json());
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Eatery Backend",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening on http://localhost:${PORT}`);
+});
