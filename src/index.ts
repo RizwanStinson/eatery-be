993137a9260ea85b0b37c0b5361880ce77d69180
Rizwan <rizwanfahim24@gmail.com>
2024-09-10 09:00:21 +0600

    dotenv ok

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import { json } from "body-parser";
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 app.use(json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
