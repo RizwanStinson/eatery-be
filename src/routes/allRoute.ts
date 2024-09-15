44e773014a63ba002e82f077ead9f6aaaf826947
hossainsmshakib <smsakib2403@gmail.com>
2024-09-15 11:26:49 +0600

    pos done

--- src/routes/allRoute.ts
+++ src/routes/allRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRouter from "./userRoute/userRoute";
 import getProfileRouter from "./userRoute/getProfile";
+import posRouter from "./posRoute/posRoute";
 
 const router = Router();
 router.use("/", userRouter);
 router.use("/", getProfileRouter);
+router.use("/", posRouter);
 
 export default router;
