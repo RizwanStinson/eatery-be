62c7783817bd7cf49ebcc54aac02567dd6d1efdb
Toushik-Ahmed <pronay5271@gmail.com>
2024-09-17 10:15:56 +0600

    pull

---
+++ src/routes/allRoute.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+// import userRouter from "./userRoute/userRoute";
+// import getProfileRouter from "./userRoute/getProfile";
+// import posRouter from "./posRoute/posroute";
+import newIngredientRoute from './inventoryRoute/ingredientRoute';
+import stockRoute from './inventoryRoute/stockRoute';
+import routerMenu from './menuRoute/menuRoute';
+
+const router = Router();
+router.use("/", userRouter);
+router.use("/", getProfileRouter);
+router.use("/", posRouter);
+
+
+
+//inventory
+router.use('/ingredient', newIngredientRoute);
+router.use('/stock', stockRoute);
+
+//menu
+router.use('/menu', routerMenu);
+
+
+
+
+export default router;
